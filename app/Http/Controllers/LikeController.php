7ad0dae0a1787847625885f8f4276688c4b7daee
ksrnnb <enrico.fermi.phys@gmail.com>
2020-09-15 07:42:49 +0000

    #34 ユーザー検索機能の追加

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -16,39 +16,6 @@ class LikeController extends Controller
         if ($is_uuid) {
             
             Like::handleLike($uuid);
-            // // みつからない場合はnull
-            // $post = Post::where('uuid', $uuid)->first();
-            // $user_id = Auth::id();
-    
-            // if (isset($post)) {
-            //     $is_liked = $post->likes->contains('user_id', $user_id);
-            //     if ($is_liked) {
-            //         Like::where('user_id', $user_id)
-            //             ->where('post_id', $post->id)
-            //             ->first()
-            //             ->delete();
-            //     } else {
-            //         Like::create([
-            //             'user_id' => $user_id,
-            //             'post_id' => $post->id,
-            //         ]);
-            //     }
-            // } else {
-            //     $comment = Comment::where('uuid', $uuid)->first();
-            //     $is_liked = $comment->likes->contains('user_id', Auth::id());
-
-            //     if ($is_liked) {
-            //         Like::where('user_id', $user_id)
-            //             ->where('comment_id', $comment->id)
-            //             ->first()
-            //             ->delete();
-            //     } else {
-            //         Like::create([
-            //             'user_id' => $user_id,
-            //             'comment_id' => $comment->id,
-            //         ]);
-            //     }
-            // }
 
         // TODO：エラーにとばす？　（uuidが送られてない場合）
         } else {
--- routes/web.php
+++ routes/web.php
@@ -18,19 +18,29 @@ Auth::routes();
 Route::get('/', 'RootController@index')->name('root');
 Route::post('/', 'Auth\LoginController@guest');
 
+Route::get('/user/search', 'UserController@search');
+Route::post('/user/search', 'UserController@find');
 Route::get('/user/{str_id}', 'UserController@index');
 Route::post('/user/{str_id}', 'UserController@action');
 Route::get('/user/{str_id}/follows', 'UserController@follows');
 Route::get('/user/{str_id}/followers', 'UserController@followers');
 
+// TODO: あとで下の通りにURLを変更したい。現状、searchというuser_idで登録したら、アクセスできなくなる
+// 　　　　テスト機能追加後の方がやりやすいと思う。
+
+// Route::get('/user/show/{str_id}', 'UserController@index');
+// Route::post('/user/show/{str_id}', 'UserController@action');
+// Route::get('/user/follows/{str_id}', 'UserController@follows');
+// Route::get('/user/followers/{str_id}', 'UserController@followers');
+
 Route::get('/home', 'HomeController@index')->middleware('auth');
 
 Route::get('/book', 'BookController@index');
 Route::post('/book', 'BookController@search');
-Route::get('/book/{isbn}', 'BookController@show')->name('book');
-Route::post('/book/{isbn}', 'BookController@post');
 Route::get('/book/post/{isbn}', 'BookController@create')->name('book_post');
 Route::post('/book/post/{isbn}', 'BookController@add');
+Route::get('/book/{isbn}', 'BookController@show')->name('book');
+Route::post('/book/{isbn}', 'BookController@post');
 
 Route::get('/post/{uuid}', 'PostController@index');
 Route::post('/post/{uuid}', 'PostController@comment');
